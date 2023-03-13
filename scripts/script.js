af2e05705af5764a510b75d2f3aefd66a3250d91
Vladislav Gurenko <v.gurenko@qiwi.com>
2023-03-14 04:32:03 +0600

    add modal window without decorative elements

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function openModal() {
+  document.querySelector('.modal-container').classList
+    .add('modal-container-open');
+}
+
+function closeModal() {
+  document.querySelector('.modal-container').classList
+    .remove('modal-container-open');
+}
