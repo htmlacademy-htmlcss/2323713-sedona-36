d13af016568864f1572fa64c5fcb3d1bd6aabdc9
Vladislav Gurenko <v.gurenko@qiwi.com>
2023-03-14 20:39:17 +0600

    finish styling modal

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
-function openModal() {
+function openModal(evt) {
   document.querySelector('.modal-container').classList
     .add('modal-container-open');
+  return false;
 }
 
 function closeModal() {
   document.querySelector('.modal-container').classList
     .remove('modal-container-open');
+  return false;
 }
